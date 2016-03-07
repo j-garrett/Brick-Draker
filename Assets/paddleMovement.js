65f139192dc95dd829a53ff7c30684106b9093aa
Jon Garrett <jonathan.garrett@gmail.com>
2016-03-06 22:21:03 -0800

    Initial Commit

---
+++ Assets/paddleMovement.js
@@ -0,0 +1,17 @@
+﻿#pragma strict
+
+var rb : Rigidbody2D;
+
+function Start () {
+	rb = GetComponent.<Rigidbody2D>();
+}
+
+function Update () {
+	if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow)) {
+		rb.velocity = Vector2(-10, 0);
+	}else if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow)) {
+		rb.velocity = Vector2(10, 0);
+	}else{
+		rb.velocity = Vector2(0, 0);
+	}
+}
