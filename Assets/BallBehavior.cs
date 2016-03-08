027040acad933e06dae0476076d9f5cb3b21185f
Mike Odom <mike.odom@smule.com>
2016-03-07 20:31:46 -0800

    Converting project to C#

---
+++ Assets/BallBehavior.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallBehavior : MonoBehaviour {
+
+    Rigidbody2D rb;
+    
+	// Use this for initialization
+	void Start () {
+	    rb = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void OnCollisionEnter2D (Collision2D coll) {
+	    GameObject touched = coll.gameObject;
+        if (touched.tag == "Player") {
+            //reverse y velocity
+            rb.velocity = new Vector2(0, 100);
+        } else if (touched.tag == "brick") {
+            //remove brick! (keep for later for reuse!)
+            touched.SetActive(false);
+            rb.velocity = new Vector2(0, -100);
+        }
+    }
+}
---
+++ Assets/PaddleBehavior.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PaddleBehavior : MonoBehaviour {
+
+    Rigidbody2D rb;
+    
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow)) {
+		    rb.velocity = new Vector2(-10, 0);
+	    } else if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow)) {
+		    rb.velocity = new Vector2(10, 0);
+	    } else {
+		    rb.velocity = new Vector2(0, 0);
+	    }
+	}
+}
